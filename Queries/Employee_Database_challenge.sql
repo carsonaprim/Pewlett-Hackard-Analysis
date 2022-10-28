1d4101ae72122e4f68e8532cea2524b98ffb3d78
Carson Prim <carsonprim@Carsons-MacBook-Air.local>
2022-10-27 22:48:51 -0400

    Final

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -26,3 +26,20 @@ FROM unique_titles
 GROUP BY title
 
 SELECT * FROM retiring_titles
+
+SELECT DISTINCT employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	employees.birth_date,
+	dept_emp.from_date,
+	dept_emp.to_date,
+	titles.title
+INTO mentorship_eligibility
+from employees
+LEFT JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+LEFT JOIN titles
+ON employees.emp_no = titles.emp_no
+WHERE dept_emp.to_date ='9999-01-01' AND employees.birth_date BETWEEN'1965-01-01' AND '1965-12-31'
+ORDER BY emp_no
+SELECT * FROM mentorship_eligibility
